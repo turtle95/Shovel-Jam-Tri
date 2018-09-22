183bbe6870cbd2d4d84f18e3391dc459e66b5983
DESKTOP-P3IPV00\hiwoods <caolam50@gmail.com>
2018-09-22 13:12:06 -0600

    planet'sgravity
    and basic obstacle collision  and health system

---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/Obstacles/Gravity.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Pull everything within range.
+/// </summary>
+public class Gravity : MonoBehaviour
+{
+    [Tooltip("Distance from planet gravity stars to take effects.")]
+    [SerializeField] private float _range;
+    [Tooltip("Maximum gravity strength.")]
+    [SerializeField] private float _maxStrength;
+    [Tooltip("Minimum gravity strength within range.")]
+    [SerializeField] private float _minStrength;
+    
+    private void FixedUpdate()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _range);
+
+        foreach (var col in colliders)
+        {
+            if (col.gameObject != gameObject)
+            {
+                Pull(col.gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Apply force to target.
+    /// The closer target object is the stronger the gravity will be.
+    /// </summary>
+    /// <param name="obj">target</param>
+    private void Pull(GameObject obj)
+    {
+        var objRigidbody = obj.GetComponent<Rigidbody>();
+        if (objRigidbody == null)
+            return;
+
+        Vector3 direction = (transform.position - obj.transform.position).normalized;
+        float distance = Vector3.Distance(transform.position, obj.transform.position);
+
+        //Points: (0, max), (range, min)
+        //actualStrength = slope * distance + maxStrength;
+        float slope = (_minStrength - _maxStrength) / _range;
+        float actualStrengh = Mathf.Clamp(slope * distance + _maxStrength, _minStrength, _maxStrength);
+        
+        objRigidbody.AddForce(objRigidbody.mass * direction * actualStrengh);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _range);
+    }
+#endif
+}
---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/Obstacles/ObstacleCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Take damange from Player (objects with "player" tag) on collision. 
+/// </summary>
+public class ObstacleCollision : MonoBehaviour
+{
+    [SerializeField] private int _damage;
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Health healthComp = collision.gameObject.GetComponent<Health>();
+            healthComp.TakeDamage(_damage);
+        }
+    }
+}
