993fd0a492a4eb287cd24f78dd1ebcb07a20995e
DESKTOP-P3IPV00\hiwoods <caolam50@gmail.com>
2018-09-22 15:12:52 -0600

    player input events
    need testing

---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/Player/MoveInput.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum MoveInputEventType
+{
+    Tap, SwipeUp, SwipeDown
+}
+
+public class MoveInputEvent : UnityEvent<MoveInputEventType> { }
+public class MoveInput : MonoBehaviour
+{
+    private struct Finger
+    {
+        public int ID;
+        public Vector2 position;
+        public TouchPhase touchPhase;
+
+        public Finger(int id, Vector3 pos, TouchPhase phase)
+        {
+            ID = id;
+            position = pos;
+            touchPhase = phase;
+        }
+    }
+
+    [SerializeField] private MoveInputEvent _onUserTouched;
+
+    [System.NonSerialized] private List<Finger> activeFingers = new List<Finger>();
+
+    void Update()
+    {
+        //touch screen stuff
+        foreach (Touch touch in Input.touches)
+        {
+            HandleTouch(new Finger(touch.fingerId, ComputeScreenToWorldPoint(touch.position), touch.phase));
+        }
+
+        if (Input.touchCount == 0)
+        {
+            //Mouse input
+            if (Input.GetMouseButtonDown(0))
+            {
+                HandleTouch(new Finger(10, ComputeScreenToWorldPoint(Input.mousePosition), TouchPhase.Began));
+            }
+            if (Input.GetMouseButton(0))
+            {
+                HandleTouch(new Finger(10, ComputeScreenToWorldPoint(Input.mousePosition), TouchPhase.Moved));
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                HandleTouch(new Finger(10, ComputeScreenToWorldPoint(Input.mousePosition), TouchPhase.Ended));
+            }
+        }
+    }
+
+    private Vector3 ComputeScreenToWorldPoint(Vector3 position)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        var point = ray.origin + (ray.direction * 10f);
+        return point;
+    }
+
+    private void HandleTouch(Finger finger)
+    {
+        switch (finger.touchPhase)
+        {
+            case TouchPhase.Began:
+                if (!activeFingers.Contains(finger))
+                {
+                    Debug.Log("touch id BEGAN " + finger.ID + " Position: " + finger.position);
+                    activeFingers.Add(finger);
+                }
+                break;
+            case TouchPhase.Ended:
+                Finger registeredFinger = activeFingers.Find(f => f.ID == finger.ID);
+                float distance = Vector3.Distance(registeredFinger.position, finger.position);
+                if (distance < 0.01f) //tolerance
+                {
+                    Debug.Log("User tap/click detected");
+                    _onUserTouched.Invoke(MoveInputEventType.Tap);
+                }
+                else
+                {
+                    Vector2 direct = finger.position - registeredFinger.position;
+
+                    if (direct.y > 0)
+                    {
+                        Debug.Log("User swiped up");
+                        _onUserTouched.Invoke(MoveInputEventType.SwipeUp);
+                    }
+                    else
+                    {
+                        Debug.Log("User swiped down");
+                        _onUserTouched.Invoke(MoveInputEventType.SwipeDown);
+                    }
+                }
+                activeFingers.Remove(registeredFinger);
+                break;
+        }
+    }
+
+}
---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/Player/TouchMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMovement : MonoBehaviour {
+
+    Vector3 touchPoint = new Vector3(5, 0, 0);
+    public Camera cam;
+    public float speed = 5;
+
+    public Transform cubeThing;
+    int xW, yW;
+
+    bool dashing = false;
+    public Rigidbody rb;
+	// Update is called once per frame
+	void Update () {
+
+        xW = Screen.width / 2;
+        yW = Screen.height / 2;
+        Touch[] myTouches = Input.touches;
+        //if(Input.GetTouch(0).position != null)
+        //
+
+        //  if (Input.GetTouch(0).phase == TouchPhase.Ended)
+        //   touchPoint = new Vector3(5,0,0);
+        touchPoint = cam.ScreenToWorldPoint(new Vector3(xW, yW, 5));
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if(myTouches[i].phase != TouchPhase.Ended)
+            {
+                touchPoint = cam.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 5));
+                //dash
+            }
+            
+            if (myTouches[i].phase == TouchPhase.Began)
+            {
+                //if(!dashing)
+                // StartCoroutine(Dash());
+                rb.AddForce(transform.forward * 6000 * Time.deltaTime, ForceMode.Impulse);
+            }
+        }
+        touchPoint.z = 0;
+        cubeThing.position = touchPoint;
+        Move();
+	}
+
+    void Move()
+    {
+        if (rb.velocity.x < 5)
+            rb.velocity = (new Vector3(6, rb.velocity.y, 0));
+        transform.LookAt(cubeThing.position);
+        //speed = 5;
+        
+    }
+
+    IEnumerator Dash()
+    {
+        dashing = true;
+        speed = 10;
+        yield return new WaitForSeconds(1f);
+        speed = 5;
+        dashing = false;
+    }
+}
