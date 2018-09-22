8e827b26da1d19b0dca226f74a53cd860a082eb2
vazor222 <vazor222@gmail.com>
2018-09-22 14:41:41 -0600

    random. ambient. whale. sound. RAWS!

---
+++ UnityFolder/Shovel Jam Tri/Assets/RandomAmbientWhaleSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAmbientWhaleSound : MonoBehaviour {
+
+	public AudioSource whaleSound;
+
+	public float intervalMax, intervalMin;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(RandomWait());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator RandomWait()
+	{
+		while (true)
+		{
+			whaleSound.Play();
+			yield return new WaitForSeconds(Random.Range(intervalMin, intervalMax));
+		}
+	}
+}
