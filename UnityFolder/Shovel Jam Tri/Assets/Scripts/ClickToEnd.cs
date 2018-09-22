77406a01dbd9a17cf87744aa8901848df272039c
vazor222 <vazor222@gmail.com>
2018-09-22 14:08:23 -0600

    added some test stuff for menu and game state, updated icon and splash icon images

---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/ClickToEnd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToEnd : MonoBehaviour {
+
+	public GameStateManager gameStateManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+	{
+		gameStateManager.GoToMainMenu();
+	}
+}
---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStateManager : MonoBehaviour {
+
+	public enum GameState
+	{
+		Init,
+		MainMenu,
+		Gameplay,
+		EndScreen
+	}
+
+	[HideInInspector]
+	public GameState gameState;
+
+	public GameObject mainMenu;
+	public GameObject mainCharacter;
+	public GameObject endScreen;
+
+	// Use this for initialization
+	void Start () {
+		gameState = GameState.Init;
+		GoToMainMenu();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void GoToMainMenu()
+	{
+		gameState = GameState.MainMenu;
+		endScreen.SetActive(false);
+		mainCharacter.SetActive(false);
+		mainMenu.SetActive(true);
+	}
+
+	public void StartGame()
+	{
+		gameState = GameState.Gameplay;
+
+		// turn off main menu and end screen just in case
+		mainMenu.SetActive(false);
+		endScreen.SetActive(false);
+
+		// turn on main character
+		mainCharacter.SetActive(true);
+	}
+}
