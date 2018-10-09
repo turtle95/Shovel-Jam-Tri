7506c4ab0783aafa76e64f41d1848cd019a1f19f
DESKTOP-P3IPV00\hiwoods <caolam50@gmail.com>
2018-10-09 17:00:00 -0600

    meteors move!

---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/Obstacles/Meteor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Meteor : MonoBehaviour {
+
+    public string targetTag;
+    public float minSpeed = 1f;
+    public float maxSpeed = 1f;
+
+    private Rigidbody rigid;
+
+	void Start () {
+
+        if (string.IsNullOrEmpty(targetTag))
+            return;
+
+        GameObject target = GameObject.FindGameObjectWithTag(targetTag);
+
+        if (target && target.activeSelf)
+        {
+            //shift actually target a bit so the meteor can get in the way of player
+            Vector3 direction = (target.transform.position - Vector3.right*5f) - transform.position;
+
+            rigid = GetComponent<Rigidbody>();
+            rigid.velocity = direction.normalized * Random.Range(minSpeed, maxSpeed);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
