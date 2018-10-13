40f32b9c409e9b3895f5f9c7d21917eaf3c1605a
Joshua Watts <joshuawatts3@gmail.com>
2018-10-13 01:59:23 -0600

    started on tutorial script

--- UnityFolder/Shovel Jam Tri/Assets/Scripts/Tutorial.cs
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/Tutorial.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour {
 
+    public bool viewedTutorial = false; //bool to determine if the tutorial has already been viewed
+
+    public TouchMovement playerScript; //ref to the player's movement script
+
+    int stageOfTut = 0;
+
+
+    public GameObject[] stageItems;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    //
+    void Update()
+    {
+        Touch[] myTouches = Input.touches; //gets an array of all the touches going on
+
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (myTouches[i].phase == TouchPhase.Began) //dash code
+            {
+                if(stageOfTut == 0)
+                {
+                    stageItems[0].SetActive(false);
+                    
+                    StartCoroutine(WaitForSomething(1));
+                }
+                if(stageOfTut == 1)
+                {
+                    playerScript.maxSpeed = 10f;
+                }
+                
+            }
+        }
+    }
+
+    IEnumerator WaitForSomething(int goal)
+    {
+        stageItems[goal].SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        stageOfTut = goal;
+        stageItems[goal].SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Collectable"))
+    }
 }
