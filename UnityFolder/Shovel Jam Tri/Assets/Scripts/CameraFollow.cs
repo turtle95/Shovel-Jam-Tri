8e13e8e02f3c8985691e8e1cea03217e7f9d536b
Joshua Watts <joshuawatts3@gmail.com>
2018-10-20 20:28:26 -0600

    Camera Delay added

--- UnityFolder/Shovel Jam Tri/Assets/Scripts/CameraFollow.cs
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/CameraFollow.cs
@@ -6,10 +6,15 @@ public class CameraFollow : MonoBehaviour {
     public int xOffset = -16;
     public int zOffset = 19;
     public Transform target;
-    Vector3 targetPos;
+    Vector3 targetPos, smoothedPos;
+	public float smoothSpeed = 0.125f;
+	Vector3 velocity = Vector3.one;
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         targetPos = new Vector3(target.position.x - xOffset, target.position.y, zOffset);
-        transform.position = targetPos;
+        
+		smoothedPos = Vector3.SmoothDamp (transform.position, targetPos, ref velocity, smoothSpeed);
+		smoothedPos = new Vector3 (targetPos.x, smoothedPos.y, targetPos.z);
+		transform.position = smoothedPos;
 	}
 }
