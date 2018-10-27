16b45102cc6804108acf1e6f96186a39260777ba
Joshua Watts <joshuawatts3@gmail.com>
2018-10-27 17:49:05 -0600

    work on tutorial

---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/MenuStuff/CanvasToWorldPos.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasToWorldPos : MonoBehaviour {
+
+    public Camera cam;
+    public RectTransform UiObject;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = cam.ScreenToWorldPoint(new Vector3(UiObject.position.x, UiObject.position.y, cam.nearClipPlane));
+	}
+}
