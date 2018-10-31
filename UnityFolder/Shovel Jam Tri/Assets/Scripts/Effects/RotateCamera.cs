3b3240bbb275bd9e235d9659c989720fdc96def1
Joshua Watts <joshuawatts3@gmail.com>
2018-10-30 23:26:35 -0600

    Fixed skybox rotation :)

---
+++ UnityFolder/Shovel Jam Tri/Assets/Scripts/Effects/RotateCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+
+    public float speed = 5f;
+    public Rigidbody playerRb;
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(-playerRb.velocity.y, -playerRb.velocity.x, 0) * Time.deltaTime /5);	
+	}
+}
